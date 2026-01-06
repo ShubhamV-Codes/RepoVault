232520128253b1147e31a56e9a7e7fa03cdb0814
Shubham Vishwakarma <sv8112004@gmail.com>
2026-01-06 16:04:15 +0530

    Login DONE

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -67,8 +67,31 @@ async function signup(req, res) {
   }
 }
 
-const login = (req, res) => {
-  res.status(200).json({ message: "User logged in" });
+async function login(req, res) {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({ message: "Email and password are required" });
+  }
+  try{
+    await connectClient();
+    const db = client.db("repovault");
+    const usersCollection = db.collection("users");
+    const user = await usersCollection.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET_KEY, { expiresIn: "1h" });
+    return res.status(200).json({ success: true, token, userId:user._id });
+  } 
+  catch (error){
+    console.error("Login error:", error);
+    return res.status(500).json({ message: "Server error" });
+  }
 };
 
 const getAllUsers = (req, res) => {
